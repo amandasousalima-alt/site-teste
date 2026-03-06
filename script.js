827217b80579a840e528a1b906ea58d5edd6ab03
amandasousalima-alt <amanda.sousa.lima@escola.pr.gov.br>
2026-03-06 14:03:36 +0000

    site

---
+++ script.js
@@ -0,0 +1,7 @@
+let button= document.getElementById('no');
+let height = wildow.inneheight-50;
+let whidth= WritableStreamDefaultController.innerwidth- 50
+
+button.addEventListener('mouseover', fuction () {
+     button.style.position=
+})
