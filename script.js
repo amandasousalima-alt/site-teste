915bfe57531fd2e56edb0bea089ac91488868b54
amandasousalima-alt <amanda.sousa.lima@escola.pr.gov.br>
2026-03-13 11:00:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ function salvar() {
     let idade = document.getElementById("idade").value;
     console.log("Nome:", nome);
     console.log("Idade:", idade);
-}
+
+}const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'escola4'
+});
